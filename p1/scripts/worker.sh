9b8bfa08eb8d68128816f1e06b44515fa14b07c7
Ghilphar <felipegaribotti@gmail.com>
2023-09-06 16:03:33 +0200

    P1 working

--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -24,4 +24,5 @@ TOKEN=$(cat /vagrant/token)
 
 # Join the k3s cluster
 #The K3s server needs port 6443 to be accessible by all nodes.
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$TOKEN sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$TOKEN INSTALL_K3S_EXEC="--node-ip=192.168.56.111 --flannel-iface=eth1" sh -
+
