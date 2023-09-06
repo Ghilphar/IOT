b75cbf1931475f4e4e2bc0ae0fa6e2ce65d9df2c
Ghilphar <felipegaribotti@gmail.com>
2023-09-06 13:42:04 +0200

    Worker server done

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,3 +1,6 @@
+# !/bin/bash
+
+# waiting for eth1 to be configured
 while true; do
     IP=$(ip addr show eth1 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)
     if [[ "$IP" == "192.168.56.110" ]]; then
@@ -7,21 +10,21 @@ while true; do
     sleep 5
 done
 
+# eth1 is configured
+# install k3s
 echo 'Starting k3s installation'
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --flannel-iface=eth1 --write-kubeconfig-mode 644" sh -
 echo 'k3s is installing'
 
-if [ ! -d /home/vagrant ]; then
-    echo 'creating /vagrant folder'
-    mkdir /vagrant
-fi
 
+# Ensure the token is present
 token_created=false
 
+# add token to shared folder
 for i in {1..20}; do
     if [ -f /var/lib/rancher/k3s/server/node-token ]; then
         echo 'k3s token is created'
-        sudo cat /var/lib/rancher/k3s/server/node-token > /home/vagrant/token
+        sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/token
         token_created=true
         break
     fi
--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
+# waiting for eth1 to be configured
+while true; do
+    IP=$(ip addr show eth1 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)
+    if [[ "$IP" == "192.168.56.111" ]]; then
+        break
+    fi
+    echo "Waiting for eth1 to be configured..."
+    sleep 2
+done
+
 # Ensure the token is present
 while true; do
-    if [[ -f "/home/vagrant/token" ]]; then
+    if [[ -f "/vagrant/token" ]]; then
         break
     fi
     echo "Waiting for k3s token from master..."
-    sleep 5
+    sleep 2
 done
 
 # Read the token
-TOKEN=$(cat /home/vagrant/token)
+TOKEN=$(cat /vagrant/token)
 
 # Join the k3s cluster
+#The K3s server needs port 6443 to be accessible by all nodes.
 curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$TOKEN sh -
