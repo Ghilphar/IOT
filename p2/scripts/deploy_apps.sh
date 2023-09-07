21f2fa86b9be550369dc903abb5123c2785f7b23
Ghilphar <felipegaribotti@gmail.com>
2023-09-07 13:09:21 +0200

    p2 done

--- p2/scripts/deploy_apps.sh
+++ p2/scripts/deploy_apps.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Ensure kubectl is in the PATH
-# export PATH=$PATH:/usr/local/bin
-# export KUBECONFIG=/home/vagrant/k3s.yaml
-
+#kubectl create namespace iot 
 kubectl apply -f /vagrant/config/app-deployments.yml
 kubectl apply -f /vagrant/config/app-services.yml
 kubectl apply -f /vagrant/config/app-ingress.yml
--- p2/scripts/nodeS_pods.sh
+++ p2/scripts/nodeS_pods.sh
@@ -11,15 +11,8 @@ done
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --flannel-iface=eth1 --write-kubeconfig-mode 644" sh -
 
-# Ensure kubectl is in the PATH for future scripts
-#export PATH=$PATH:/usr/local/bin
-# Allow vagrant user to access kubectl
-#cp /etc/rancher/k3s/k3s.yaml /home/vagrant/
-#chown vagrant:vagrant /home/vagrant/k3s.yaml
-#export KUBECONFIG=/home/vagrant/k3s.yaml
-
-# Waiting for Kubernetes API to be responsive
+# Waiting for Kubernetes API to be available
 while ! kubectl cluster-info; do
-    echo "Waiting for Kubernetes API to be responsive..."
+    echo "Waiting for Kubernetes API to be available..."
     sleep 5
 done
