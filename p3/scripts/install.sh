45158f2ffcda80f97dbfca2b0c8b649713c08f02
Ghilphar <felipegaribotti@gmail.com>
2023-09-07 21:28:17 +0200

    cluster running

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -6,15 +6,6 @@ sudo apt install -y docker.io
 sudo groupadd docker
 sudo usermod -aG docker fgaribot
 
-# install latest release of kubectl
-if [ ! -f /usr/local/bin/kubectl ] ; then
-    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-    echo 'alias k="/usr/local/bin/kubectl"' >> /home/$USER/.bashrc
-    source /home/$USER/.bashrc
-    rm kubectl
-fi
-
 # install latest release of k3d
 if [ ! -f /usr/local/bin/k3d ] ; then
     curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
 # Exit immediately if a command exits with a non-zero status.
-set -e
+# set -e
 
-echo ">>> Creating a Kubernetes cluster with k3d"
-# create cluster p3
-# expose services on port 80 and 443
+#echo ">>> Creating a Kubernetes cluster with k3d"
+if k3d cluster list | grep -q 'p3'; then
+    echo ">>> Cluster 'p3' already exists. Using existing cluster."
+else
+    echo ">>> Creating a Kubernetes cluster with k3d"
+    # create cluster p3
+    k3d cluster create p3 \
+      --api-port 6443 \
+      --servers 1 \
+      --agents 2 \
+      --port 8080:80@loadbalancer \
+      --port 8443:443@loadbalancer \
+      --wait \
+      --verbose
+fi
+# expose services on localhost (port 8080 for HTTP and 8443 for HTTPS)
+
+
 # wait for cluster to be ready
-k3d cluster create p3 \
-  --api-port 6443 \
-  --servers 1 \
-  --agents 2 \
-  --port 8080:80@loadbalancer \
-  --port 8443:443@loadbalancer \
-  --wait \
-  --verbose
-
 echo ">>> Waiting for Traefik (Ingress controller) to set up"
 # Wait for Traefik related jobs to complete (with a maximum of 5 minutes timeout)
 kubectl -n kube-system wait --for=condition=complete --timeout=5m jobs/helm-install-traefik-crd
@@ -23,14 +29,23 @@ kubectl -n kube-system wait --for=condition=complete --timeout=5m jobs/helm-inst
 kubectl get jobs -n kube-system
 
 echo ">>> Setting up namespaces"
-kubectl create namespace argocd
-kubectl create namespace dev
+# Check if argocd namespace exists
+if ! kubectl get namespace argocd > /dev/null 2>&1; then
+  kubectl create namespace argocd
+fi
+
+# Check if dev namespace exists
+if ! kubectl get namespace dev > /dev/null 2>&1; then
+  kubectl create namespace dev
+fi
 
 echo ">>> Installing ArgoCD"
-kubectl apply -f ../config/install_argocd.yaml -n argocd
+#kubectl apply -f ../config/install_argocd.yaml -n argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 # waiting for all pods to be ready
 kubectl wait --for=condition=Ready --timeout=5m pods --all -n argocd
-kubectl apply -f ../config/argocd_ingress.yaml -n argocd
+
+kubectl apply -f ../config/ingress_argocd.yaml -n argocd
 # check ArgoCD installation status
 kubectl rollout status deployment argocd-server -n argocd
 kubectl rollout status deployment argocd-redis -n argocd
